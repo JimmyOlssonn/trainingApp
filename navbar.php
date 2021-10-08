5337ad40cadf61f340920a462ba426e4f330afdf
JimmyOlssonn <olssonjimmy98@gmail.com>
2021-10-08 22:36:25 +0200

    New navbar
    Simplefied the navbar and deleted the dropdown.

--- navbar.php
+++ navbar.php
@@ -2,29 +2,30 @@
   Navbar 
   Include with: include_once("navbar.php") 
  -->
-<div class="navbar">
-  <a href="#home">Hem</a>
-  <a href="#about">Om oss</a>
-  <a href="#price">Priser</a>
-  <?php
-  if (isset($_SESSION['Username'])) { // If a user is logged in
-    echo '
-            <div class="dropdown">
-              <a class="dropdown-button">' . $_SESSION['Username'] . '</a>
-              <div class="dropdown-container">
-                <a href="profile.php">Profile</a>
-                <a href="#">Runs</a>
-                <a href="#">Membership</a>
-                <a href="#">Settings</a>
-                <a href="logout.php">Sign out</a>
-              </div>
-            </div>
-            ';
-  } else { // If no user is logged in
-    echo '
-    <a onclick="toggleRegModal()">Registrera</a>
-    <a onclick="toggleLoginModal()">Logga in</a>
+<header class="navbar">
+  <a href="/trainingApp">trainingApp</a>
+  <nav>
+    <ul class="nav-links">
+      <li><a href="#">Tjänster</a></li>
+      <li><a href="#">Priser</a></li>
+      <li><a href="#">Om oss</a></li>
+    </ul>
+  </nav>
+  <div>
+    <?php
+    if (isset($_SESSION['Username'])) { // If a user is logged in
+      echo '
+        <ul class="nav-links">
+          <li><a href="profile.php">' . $_SESSION['Username'] . '</a></li>
+          <li><a href="logout.php">Logga ut</a></li>
+        </ul>
           ';
-  }
-  ?>
-</div>
+    } else { // If no user is logged in
+      echo '
+        <button onclick="toggleLoginModal()">Logga in</button>
+        <button onclick="toggleRegModal()">Registrera</button>
+        ';
+    }
+    ?>
+  </div>
+</header>
